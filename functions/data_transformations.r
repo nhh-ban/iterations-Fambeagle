61646b18c688970aaa83c604ca484315b01d5acf
Fambeagle <146773265+Fambeagle@users.noreply.github.com>
2023-10-16 22:44:49 +0200

    Update data_transformations.r

--- functions/data_transformations.r
+++ functions/data_transformations.r
@@ -6,12 +6,14 @@ transform_metadata_to_df <- function(stations_metadata) {
     bind_rows() %>% 
     mutate(
       latestData = map_chr(latestData, 1, .default = NA_character_),
-      latestData = as_datetime(latestData, tz = "Europe/Berlin")
-    ) %>% 
-    mutate(
+      latestData = as_datetime(latestData, tz = "Europe/Berlin"),
       latestData = with_tz(latestData, tzone = "UTC")
-    )
+    ) %>% 
+    select(id, name, latestData, lat, lon) # I got the fail on the test so i needed to add this to the function to get it through 
   
   return(df)
 }
+
+
+
 
