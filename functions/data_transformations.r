88a1915122e1d108c45a26dea7c27b76584f4e00
Fambeagle <146773265+Fambeagle@users.noreply.github.com>
2023-10-17 00:28:44 +0200

    Update data_transformations.r

--- functions/data_transformations.r
+++ functions/data_transformations.r
@@ -1,19 +1,42 @@
+# I got the fail on the test so i needed to add a few things to make it go thorugh
+# One thing that was hard to figure out was that there was lat and lon deep inside the location list
+# pretty fun
 
 transform_metadata_to_df <- function(stations_metadata) {
   
+  # took some inspiration from the lecture notes
   df <- stations_metadata[[1]] %>% 
     map(as_tibble) %>% 
     bind_rows() %>% 
     mutate(
-      latestData = map_chr(latestData, 1, .default = NA_character_),
+      latestData = map_chr(latestData, 1, .default = ""),
       latestData = as_datetime(latestData, tz = "Europe/Berlin"),
-      latestData = with_tz(latestData, tzone = "UTC")
+      location = map(location, unlist),
+      lat = map_dbl(location, "latLon.lat"),
+      lon = map_dbl(location, "latLon.lon")
     ) %>% 
-    select(id, name, latestData, lat, lon) # I got the fail on the test so i needed to add this to the function to get it through 
+    select(-location) %>% 
+    mutate(latestData = with_tz(latestData, tzone = "UTC")) %>% 
+    select(id, name, latestData, lat, lon)
   
   return(df)
 }
 
+# What i did not expect with API data was the amount of list it actually is since ive never used that type of data
 
 
+transform_volumes <- function(json_data) {
+  volume_data <- json_data$data$trafficData$volume$byHour$edges
+  
+  from_times <- map_chr(volume_data, ~ .x$node$from)
+  to_times <- map_chr(volume_data, ~ .x$node$to)
+  volumes <- map_dbl(volume_data, ~ .x$node$total$volumeNumbers$volume)
+  df <- tibble(
+    from = ymd_hms(from_times),
+    to = ymd_hms(to_times),
+    volume = volumes
+  )
+  
+  return(df)
+}
 
