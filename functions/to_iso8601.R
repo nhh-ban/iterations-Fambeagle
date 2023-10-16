db589ab3783e980e1960ec7cefec5468ee48b927
Fambeagle <146773265+Fambeagle@users.noreply.github.com>
2023-10-16 23:48:09 +0200

    Update to_iso8601.R

--- functions/to_iso8601.R
+++ functions/to_iso8601.R
@@ -1,8 +1,8 @@
 
 to_iso8601 <- function(date_time, offsetdays) {
- 
   date <- date_time + days(offsetdays)
+  iso_date <- iso8601(date)
   
-  iso_date <- iso8601(date, use_tz = TRUE)
   return(paste0(substr(iso_date, 1, 19), "Z"))
 }
+
